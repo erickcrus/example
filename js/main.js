adbcb83c24315f11128fc4735941ed752265911d
Erick Crus <35431617+erickcrus@users.noreply.github.com>
2021-09-17 13:04:31 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+    const frases = [
+        'Que os sonhos sejam leves e a noite cheia de paz!',
+        'Que a paz do nosso senhor Jesus Cristo esteja conosco',
+        'Um pedaço de pão comido em paz é melhor do que um banquete comido com ansiedade',
+        'Ninguém será generoso se não for, ao mesmo tempo, justo',
+        'A grandeza não consiste em receber honras, mas merecê-las',
+        'O passado é um ótimo professor'
+    ];
+    var frase = $('#frase');
+    var timer = $('#timer');
+
+    const initTimer = (time, callback = false) => { }
+
+    const startScript = () => { }
+
+    initTimer(10, startScript);
+});
